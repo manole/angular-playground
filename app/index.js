f740a81e128e9a75a6f2ec018ec5ebb56407a0aa
Manole <manole.rotaru@finra.org>
2016-05-24 23:18:37 -0400

    add service call

--- app/index.js
+++ app/index.js
@@ -9,8 +9,24 @@ let gadgetTemplate = require('./gadget.tpl');
 
 // module definition
 module.exports = angular.module('cvent.weather.gadget', [md])
-    .controller('weatherController', function() {
-        this.someProperty = 'cvent';
+
+    .service('weatherService', function($http)  {
+        let options = {
+            method: 'GET',
+            url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22fairfax%2C%20va%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+        };
+
+        this.getWeatherData = () => {
+            return $http(options);
+        }
+
+    })
+    .controller('weatherController', function(weatherService) {
+        this.weatherData = {};
+
+        weatherService.getWeatherData().then((data) => {
+            this.weatherData = data;
+        });
     })
     .component('weatherGadget', {
         template: gadgetTemplate,
